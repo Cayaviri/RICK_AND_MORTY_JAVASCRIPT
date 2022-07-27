abd261e75c9ff232586320bc8f0c772849a9a24a
Caleb Ayaviri Ocampo <83101963+Cayaviri@users.noreply.github.com>
2022-07-27 14:43:37 -0400

    primer

--- index.js
+++ index.js
@@ -5,7 +5,23 @@ fetch(url)
     console.log(dato)
     let elementos= document.getElementById('elemento')
     elementos.innerHTML=  dato.results.map(
-        dato=>`<p> ${dato.name}</p> <img src='${dato.image}'>`
+        dato=>`
+            <div class="card mb-3" style="max-width: 540px;">
+            <div class="row">
+                <div class="col-md-4">
+                <img src="${dato.image}" class="img-fluid rounded-start" alt="...">
+                </div>
+                <div class="col-md-8">
+                <div class="card-body">
+                    <h5 class="card-title text-bold">${dato.name}</h5>
+                    <p class="card-text">${dato.status} - ${dato.species}</p>
+                    <p class="card-text">Ultima ubicacion: ${dato.origin.name}</p>
+                    <p class="card-text"><small class="text-muted">${dato.location.name}</small></p>
+                </div>
+                </div>
+            </div>
+            </div>
+        `
     )
 })
 .catch(error => console.log(error))
